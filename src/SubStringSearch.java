3fc38f8ee0a041c4cf5a851e3b852b047e24658b
Johannes Willemse Moolman <26892154@sun.ac.za>
2024-05-11 21:05:58 +0200

    DFA construction for KMP search moved to KMP class

--- src/SubStringSearch.java
+++ src/SubStringSearch.java
@@ -27,39 +27,48 @@ class BruteForceSubStringSearch implements SubStringSearch {
 }
 class KMPSubStringSearch implements SubStringSearch {
 
-    private String pattern;
-    private int[][] dfa;
+    class KMP {
+
+        private String pattern;
+        private int[][] dfa;
+
+        public int search(String txt) {
+            int i, j, N = txt.length(), M = pattern.length();
+            for (i = 0, j = 0; i < N && j < M; i++) {
+                j = dfa[txt.charAt(i)][j];
+            }
+            if (j == M) {
+                return i - M;
+            }
+            return N;
+        }
+    
+        public KMP(String pattern) {
+            this.pattern = pattern;
+            int M = pattern.length();
+            int R = 256;
+            dfa = new int[R][M];
+            dfa[pattern.charAt(0)][0] = 1;
+            for (int X = 0, j = 1; j < M; j++) {
+                for (int c = 0; c < R; c++) {
+                    dfa[c][j] = dfa[c][X];
+                }
+                dfa[pattern.charAt(j)][j] = j + 1;
+                X = dfa[pattern.charAt(j)][X];
+            }
+    
+        }
+
+    }
 
     @Override
     public int search(String pattern, String txt) {
-        return 0;
+        KMP kmp = new KMP(pattern);
+        int offset = kmp.search(txt);
+        return offset;
     }
 
-    private int search(String txt) {
-        int i, j, N = txt.length(), M = pattern.length();
-        for (i = 0, j = 0; i < N && j < M; i++) {
-            j = dfa[txt.charAt(i)][j];
-        }
-        if (j == M) {
-            return i - M;
-        }
-        return N;
-    }
 
-    public void KMP(String pattern) {
-        this.pattern = pattern;
-        int M = pattern.length();
-        int R = 256;
-        dfa = new int[R][M];
-        dfa[pattern.charAt(0)][0] = 1;
-        for (int X = 0, j = 1; j < M; j++) {
-            for (int c = 0; c < R; c++) {
-                dfa[c][j] = dfa[c][X];
-            }
-            dfa[pattern.charAt(j)][j] = j + 1;
-            X = dfa[pattern.charAt(j)][X];
-        }
-
-    }
 }
+
 
